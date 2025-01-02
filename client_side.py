be7ea204bbe760186be652a49337e26dd74f0644
Felipe Escalona <escalonaf12@gmail.com>
2025-01-02 17:05:27 -0400

    Simple two way chat

--- client_side.py
+++ client_side.py
@@ -17,3 +17,22 @@ client_socket= socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 
 
 client_socket.connect( (dest_ip,dest_port))
+
+
+#Send and recieve messages
+while True:
+    message= client_socket.recv(bytes_size).decode(encoder)
+
+    #Quit if the coneected server wants to quite
+
+    if message=="quit":
+        client_socket.send("quit".encode(encoder))
+        print("\n ending the chat...")
+        break
+    else:
+        print(f"\n {message} ")
+        message = input("Message: ")
+        client_socket.send(message.encode(encoder))
+
+
+client_socket.close()
--- server_side.py
+++ server_side.py
@@ -23,9 +23,29 @@ server_socket.listen()
 
 #Accept incoming connection and they are connecter
 
-print(f"Server is running ... in {host_ip}:{host_port} /n ")
+print(f"Server is running ... in {host_ip}:{host_port} \n ")
 
 client_socket,client_address=server_socket.accept()
 
-client_socket.send("You are connected.../n".encode(encoder))
+client_socket.send("You are connected...\n".encode(encoder))
 
+#Send recieve messages
+
+while True:
+    #Recieve information
+    message=client_socket.recv(byte_size).decode(encoder)
+
+    #Quit if client wants to quit
+    if message== "quit":
+        client_socket.send("quit".encode(encoder))
+        print("ending the chat...")
+        break
+    else:
+
+        print(f"\n{message}")
+        message=input("Message: ")
+        client_socket.send(message.encode(encoder))
+
+
+
+client_socket.close()
